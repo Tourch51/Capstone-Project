6de1f14016aecdf92a67478a8e444cabfb1bc02d
Connor cheung <calebchang991@outlook.com>
2019-04-28 21:38:48 -0400

    Add project files.

---
+++ CapstoneProject/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace CapstoneProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Title = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.txtbox_Multiplysign = new System.Windows.Forms.TextBox();
+            this.txt_Matrix21 = new System.Windows.Forms.TextBox();
+            this.txt_Matrix22 = new System.Windows.Forms.TextBox();
+            this.txt_Matrix23 = new System.Windows.Forms.TextBox();
+            this.Txt_Equals = new System.Windows.Forms.TextBox();
+            this.txt_Answer1 = new System.Windows.Forms.TextBox();
+            this.txt_Answer4 = new System.Windows.Forms.TextBox();
+            this.txt_Answer7 = new System.Windows.Forms.TextBox();
+            this.txt_Answer2 = new System.Windows.Forms.TextBox();
+            this.txt_Answer5 = new System.Windows.Forms.TextBox();
+            this.txt_Answer8 = new System.Windows.Forms.TextBox();
+            this.txt_Answer3 = new System.Windows.Forms.TextBox();
+            this.txt_Answer6 = new System.Windows.Forms.TextBox();
+            this.txt_Answer9 = new System.Windows.Forms.TextBox();
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.txt_Error = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbl_Title
+            // 
+            this.lbl_Title.AutoSize = true;
+            this.lbl_Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Title.ForeColor = System.Drawing.Color.Blue;
+            this.lbl_Title.Location = new System.Drawing.Point(379, 28);
+            this.lbl_Title.Name = "lbl_Title";
+            this.lbl_Title.Size = new System.Drawing.Size(202, 31);
+            this.lbl_Title.TabIndex = 0;
+            this.lbl_Title.Text = "Matrix Mulitplier";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(79, 226);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(35, 30);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(79, 289);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(35, 30);
+            this.textBox4.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(227, 226);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(35, 30);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(157, 225);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(35, 30);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(157, 288);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(35, 30);
+            this.textBox5.TabIndex = 5;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox8.Location = new System.Drawing.Point(157, 349);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(35, 30);
+            this.textBox8.TabIndex = 6;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox7.Location = new System.Drawing.Point(79, 348);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(35, 30);
+            this.textBox7.TabIndex = 7;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox6.Location = new System.Drawing.Point(227, 288);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(35, 30);
+            this.textBox6.TabIndex = 8;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox9.Location = new System.Drawing.Point(227, 348);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(35, 30);
+            this.textBox9.TabIndex = 9;
+            // 
+            // txtbox_Multiplysign
+            // 
+            this.txtbox_Multiplysign.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtbox_Multiplysign.Location = new System.Drawing.Point(322, 289);
+            this.txtbox_Multiplysign.Name = "txtbox_Multiplysign";
+            this.txtbox_Multiplysign.Size = new System.Drawing.Size(19, 30);
+            this.txtbox_Multiplysign.TabIndex = 10;
+            this.txtbox_Multiplysign.Text = "X";
+            // 
+            // txt_Matrix21
+            // 
+            this.txt_Matrix21.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Matrix21.Location = new System.Drawing.Point(400, 225);
+            this.txt_Matrix21.Name = "txt_Matrix21";
+            this.txt_Matrix21.Size = new System.Drawing.Size(30, 30);
+            this.txt_Matrix21.TabIndex = 11;
+            // 
+            // txt_Matrix22
+            // 
+            this.txt_Matrix22.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Matrix22.Location = new System.Drawing.Point(400, 288);
+            this.txt_Matrix22.Name = "txt_Matrix22";
+            this.txt_Matrix22.Size = new System.Drawing.Size(30, 30);
+            this.txt_Matrix22.TabIndex = 12;
+            // 
+            // txt_Matrix23
+            // 
+            this.txt_Matrix23.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Matrix23.Location = new System.Drawing.Point(400, 348);
+            this.txt_Matrix23.Name = "txt_Matrix23";
+            this.txt_Matrix23.Size = new System.Drawing.Size(30, 30);
+            this.txt_Matrix23.TabIndex = 13;
+            // 
+            // Txt_Equals
+            // 
+            this.Txt_Equals.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Txt_Equals.Location = new System.Drawing.Point(525, 289);
+            this.Txt_Equals.Name = "Txt_Equals";
+            this.Txt_Equals.Size = new System.Drawing.Size(28, 30);
+            this.Txt_Equals.TabIndex = 14;
+            this.Txt_Equals.Text = " =";
+            // 
+            // txt_Answer1
+            // 
+            this.txt_Answer1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Answer1.Location = new System.Drawing.Point(617, 225);
+            this.txt_Answer1.Name = "txt_Answer1";
+            this.txt_Answer1.Size = new System.Drawing.Size(52, 30);
+            this.txt_Answer1.TabIndex = 15;
+            // 
+            // txt_Answer4
+            // 
+            this.txt_Answer4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Answer4.Location = new System.Drawing.Point(617, 288);
+            this.txt_Answer4.Name = "txt_Answer4";
+            this.txt_Answer4.Size = new System.Drawing.Size(52, 30);
+            this.txt_Answer4.TabIndex = 16;
+            // 
+            // txt_Answer7
+            // 
+            this.txt_Answer7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Answer7.Location = new System.Drawing.Point(617, 348);
+            this.txt_Answer7.Name = "txt_Answer7";
+            this.txt_Answer7.Size = new System.Drawing.Size(52, 30);
+            this.txt_Answer7.TabIndex = 17;
+            // 
+            // txt_Answer2
+            // 
+            this.txt_Answer2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Answer2.Location = new System.Drawing.Point(703, 225);
+            this.txt_Answer2.Name = "txt_Answer2";
+            this.txt_Answer2.Size = new System.Drawing.Size(52, 30);
+            this.txt_Answer2.TabIndex = 18;
+            // 
+            // txt_Answer5
+            // 
+            this.txt_Answer5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Answer5.Location = new System.Drawing.Point(703, 288);
+            this.txt_Answer5.Name = "txt_Answer5";
+            this.txt_Answer5.Size = new System.Drawing.Size(52, 30);
+            this.txt_Answer5.TabIndex = 19;
+            // 
+            // txt_Answer8
+            // 
+            this.txt_Answer8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Answer8.Location = new System.Drawing.Point(703, 348);
+            this.txt_Answer8.Name = "txt_Answer8";
+            this.txt_Answer8.Size = new System.Drawing.Size(52, 30);
+            this.txt_Answer8.TabIndex = 20;
+            // 
+            // txt_Answer3
+            // 
+            this.txt_Answer3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Answer3.Location = new System.Drawing.Point(798, 225);
+            this.txt_Answer3.Name = "txt_Answer3";
+            this.txt_Answer3.Size = new System.Drawing.Size(52, 30);
+            this.txt_Answer3.TabIndex = 21;
+            // 
+            // txt_Answer6
+            // 
+            this.txt_Answer6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Answer6.Location = new System.Drawing.Point(798, 288);
+            this.txt_Answer6.Name = "txt_Answer6";
+            this.txt_Answer6.Size = new System.Drawing.Size(52, 30);
+            this.txt_Answer6.TabIndex = 22;
+            // 
+            // txt_Answer9
+            // 
+            this.txt_Answer9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Answer9.Location = new System.Drawing.Point(798, 348);
+            this.txt_Answer9.Name = "txt_Answer9";
+            this.txt_Answer9.Size = new System.Drawing.Size(52, 30);
+            this.txt_Answer9.TabIndex = 23;
+            // 
+            // btn_1
+            // 
+            this.btn_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_1.Location = new System.Drawing.Point(434, 453);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(110, 56);
+            this.btn_1.TabIndex = 24;
+            this.btn_1.Text = "GO!";
+            this.btn_1.UseVisualStyleBackColor = true;
+            this.btn_1.Click += new System.EventHandler(this.btn_1_Click);
+            // 
+            // txt_Error
+            // 
+            this.txt_Error.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Error.Location = new System.Drawing.Point(174, 139);
+            this.txt_Error.Name = "txt_Error";
+            this.txt_Error.Size = new System.Drawing.Size(629, 30);
+            this.txt_Error.TabIndex = 25;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.BackgroundImage = global::CapstoneProject.Properties.Resources.Wood_Wallpaper_Background_4;
+            this.ClientSize = new System.Drawing.Size(962, 552);
+            this.Controls.Add(this.txt_Error);
+            this.Controls.Add(this.btn_1);
+            this.Controls.Add(this.txt_Answer9);
+            this.Controls.Add(this.txt_Answer6);
+            this.Controls.Add(this.txt_Answer3);
+            this.Controls.Add(this.txt_Answer8);
+            this.Controls.Add(this.txt_Answer5);
+            this.Controls.Add(this.txt_Answer2);
+            this.Controls.Add(this.txt_Answer7);
+            this.Controls.Add(this.txt_Answer4);
+            this.Controls.Add(this.txt_Answer1);
+            this.Controls.Add(this.Txt_Equals);
+            this.Controls.Add(this.txt_Matrix23);
+            this.Controls.Add(this.txt_Matrix22);
+            this.Controls.Add(this.txt_Matrix21);
+            this.Controls.Add(this.txtbox_Multiplysign);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbl_Title);
+            this.ForeColor = System.Drawing.Color.SteelBlue;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Title;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox txtbox_Multiplysign;
+        private System.Windows.Forms.TextBox txt_Matrix21;
+        private System.Windows.Forms.TextBox txt_Matrix22;
+        private System.Windows.Forms.TextBox txt_Matrix23;
+        private System.Windows.Forms.TextBox Txt_Equals;
+        private System.Windows.Forms.TextBox txt_Answer1;
+        private System.Windows.Forms.TextBox txt_Answer4;
+        private System.Windows.Forms.TextBox txt_Answer7;
+        private System.Windows.Forms.TextBox txt_Answer2;
+        private System.Windows.Forms.TextBox txt_Answer5;
+        private System.Windows.Forms.TextBox txt_Answer8;
+        private System.Windows.Forms.TextBox txt_Answer3;
+        private System.Windows.Forms.TextBox txt_Answer6;
+        private System.Windows.Forms.TextBox txt_Answer9;
+        private System.Windows.Forms.Button btn_1;
+        private System.Windows.Forms.TextBox txt_Error;
+    }
+}
+
---
+++ CapstoneProject/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapstoneProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_1_Click(object sender, EventArgs e)
+        {
+
+            //********************************
+            // Application: Matrix Multiplier
+            // Designer: Cheung Connor
+            // Date: 4/27/19
+            //********************************
+            bool moveFoward = true;
+            double result;
+
+            if (!double.TryParse(textBox1.Text, out result))
+            {
+                txt_Error.Text = "ERROR => MUST ENTER NUMBERS";
+                txt_Error.ForeColor = Color.Red;
+                moveFoward = false;
+            }
+            if (!double.TryParse(textBox2.Text, out result))
+            {
+                txt_Error.Text = "ERROR => MUST ENTER NUMBERS";
+                txt_Error.ForeColor = Color.Red;
+                moveFoward = false;
+            }
+            if (!double.TryParse(textBox3.Text, out result))
+            {
+                txt_Error.Text = "ERROR => MUST ENTER NUMBERS";
+                txt_Error.ForeColor = Color.Red;
+                moveFoward = false;
+            }
+            if (!double.TryParse(textBox4.Text, out result))
+            {
+                txt_Error.Text = "ERROR => MUST ENTER NUMBERS";
+                txt_Error.ForeColor = Color.Red;
+                moveFoward = false;
+            }
+            if (!double.TryParse(textBox5.Text, out result))
+            {
+                txt_Error.Text = "ERROR => MUST ENTER NUMBERS";
+                txt_Error.ForeColor = Color.Red;
+                moveFoward = false;
+            }
+            if (!double.TryParse(textBox6.Text, out result))
+            {
+                txt_Error.Text = "ERROR => MUST ENTER NUMBERS";
+                txt_Error.ForeColor = Color.Red;
+                moveFoward = false;
+            }
+            if (!double.TryParse(textBox7.Text, out result))
+            {
+                txt_Error.Text = "ERROR => MUST ENTER NUMBERS";
+                txt_Error.ForeColor = Color.Red;
+                moveFoward = false;
+            }
+            if (!double.TryParse(textBox8.Text, out result))
+            {
+                txt_Error.Text = "ERROR => MUST ENTER NUMBERS";
+                txt_Error.ForeColor = Color.Red;
+                moveFoward = false;
+            }
+            if (!double.TryParse(textBox9.Text, out result))
+            {
+                txt_Error.Text = "ERROR => MUST ENTER NUMBERS";
+                txt_Error.ForeColor = Color.Red;
+                moveFoward = false;
+            }
+            if (!double.TryParse(txt_Matrix21.Text, out result))
+            {
+                txt_Error.Text = "ERROR => MUST ENTER NUMBERS";
+                txt_Error.ForeColor = Color.Red;
+                moveFoward = false;
+            }
+            if (!double.TryParse(txt_Matrix22.Text, out result))
+            {
+                txt_Error.Text = "ERROR => MUST ENTER NUMBERS";
+                txt_Error.ForeColor = Color.Red;
+                moveFoward = false;
+            }
+            if (!double.TryParse(txt_Matrix23.Text, out result))
+            {
+                txt_Error.Text = "ERROR => MUST ENTER NUMBERS";
+                txt_Error.ForeColor = Color.Red;
+                moveFoward = false;
+            }
+
+
+            if (moveFoward == true)
+            {
+                txt_Error.Text = " ";
+                double Box1 = double.Parse(textBox1.Text);
+                double Box4 = double.Parse(textBox4.Text);
+                double Box7 = double.Parse(textBox7.Text);
+                double Box2 = double.Parse(textBox2.Text);
+                double Box5 = double.Parse(textBox5.Text);
+                double Box8 = double.Parse(textBox8.Text);
+                double Box3 = double.Parse(textBox3.Text);
+                double Box6 = double.Parse(textBox6.Text);
+                double Box9 = double.Parse(textBox9.Text);
+                double matrix21 = double.Parse(txt_Matrix21.Text);
+                double matrix22 = double.Parse(txt_Matrix22.Text);
+                double matrix23 = double.Parse(txt_Matrix23.Text);
+
+
+                double answer1 = Box1 * matrix21;
+                txt_Answer1.Text = answer1.ToString();
+
+                double answer2 = Box2 * matrix22;
+                txt_Answer2.Text = answer2.ToString();
+
+                double answer3 = Box3 * matrix23;
+                txt_Answer3.Text = answer3.ToString();
+
+                double answer4 = Box4 * matrix21;
+                txt_Answer4.Text = answer4.ToString();
+
+                double answer5 = Box5 * matrix22;
+                txt_Answer5.Text = answer5.ToString();
+
+                double answer6 = Box6 * matrix23;
+                txt_Answer6.Text = answer6.ToString();
+
+                double answer7 = Box7 * matrix21;
+                txt_Answer7.Text = answer7.ToString();
+
+                double answer8 = Box8 * matrix22;
+                txt_Answer8.Text = answer8.ToString();
+
+                double answer9 = Box9 * matrix23;
+                txt_Answer9.Text = answer9.ToString();
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
